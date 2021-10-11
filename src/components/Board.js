e4f8c37e49dc430cf28a195586c247ebfd6ef34c
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-10-11 11:34:38 +0530

    Fix communication method

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,32 +1,40 @@
-import React from 'react';
-import Switch from './Switch';
+import React from "react";
+import Switch from "./Switch";
 
-class Board extends React.Component{
+class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            is_on: Object.keys(this.props.switches).map((id) =>
-                (id: false))
-        }
+            is_on: Object.keys(this.props.switches).map((id) => (id: false)),
+        };
         this.props.ws.onmessage = this.handleWebhook;
     }
     handleWebhook = (event) => {
         var data = JSON.parse(event.data);
         var is_on = this.state.is_on;
-        is_on[data.id] = data.is_on;
-        this.setState({"is_on": is_on});
-    }
+        var state_on;
+        if (data.state === "on") {
+            state_on = true;
+        } else if (data.state === "off") {
+            state_on = false;
+        }
+        is_on[data.id] = state_on;
+        this.setState({ is_on: is_on });
+    };
     toggle = (e, id) => {
-        var data = {id: id, is_on: !this.state.is_on[id]};
+        var data = { id: id, toggle: this.state.is_on[id] ? "off" : "on" };
         this.props.ws.send(JSON.stringify(data));
-    }
-    render(){
-        const switch_comps = Object.keys(this.props.switches).map((id) =>
-            <Switch id={id} name={this.props.switches[id]} toggle={this.toggle} is_on={this.state.is_on[id]}/>
-        );
-        return ( 
-		<div className = "switch-container"> {switch_comps} </div>
-        )
+    };
+    render() {
+        const switch_comps = Object.keys(this.props.switches).map((id) => (
+            <Switch
+                id={id}
+                name={this.props.switches[id]}
+                toggle={this.toggle}
+                is_on={this.state.is_on[id]}
+            />
+        ));
+        return <div className="switch-container"> {switch_comps} </div>;
     }
 }
 export default Board;
