3807fec9186eaf37b7f302e62f8d1f77e725aebd
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-10-11 10:06:43 +0530

    Use loop to create multiple switches

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Board from './components/Board.js';
 
-class App extends React.Component{
-constructor(props){
-	super(props);
-	this.ws = new WebSocket("ws://127.0.0.1:4444");
-}
-render(){
-return (
-	<Board ws={this.ws} />
-)
-}
+const switches = {
+    "1": "Switch 1",
+    "2": "Switch 2",
+    "3": "Switch 3"
+};
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.ws = new WebSocket("ws://127.0.0.1:4444");
+    }
+    render() {
+        return (
+		<Board ws={this.ws} switches={switches}/>
+        )
+    }
 }
 export default App;
