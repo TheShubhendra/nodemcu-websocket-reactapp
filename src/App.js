f2766f4c9540dbf1856d44a9387babe71022328b
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-10-24 13:46:37 +0530

    Increase number of switches upto 9

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from 'react';
 import Board from './components/Board.js';
 
 const switches = {
+    "0": "Switch 0",
     "1": "Switch 1",
     "2": "Switch 2",
-    "3": "Switch 3"
+    "3": "Switch 3",
+    "4": "Switch 4",
+    "5": "Switch 5",
+    "6": "Switch 6",
+    "7": "Switch 7",
+    "8": "Switch 8",
 };
 
 class App extends React.Component {
